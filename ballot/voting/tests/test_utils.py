c9bc6a3a995f9562cfe376d3fda9c8c599a78a06
FedeG <federicogonzalez761@gmail.com>
2023-11-17 19:17:18 -0300

    Update tests to use parametrize an reduce duplicated code

--- ballot/voting/tests/test_utils.py
+++ ballot/voting/tests/test_utils.py
@@ -3,19 +3,21 @@ import pytest
 from voting.utils import has_voted, has_voted_percentage
 from .factories import VoterFactory
 
-
-@pytest.mark.django_db
-def test_has_voted_true():
-    voter = VoterFactory(has_voted=True)
-
-    assert has_voted(voter.dni) is True
+# has_voted tests
 
 
 @pytest.mark.django_db
-def test_has_voted_false():
-    voter = VoterFactory(has_voted=False)
+@pytest.mark.parametrize(
+    'has_voted_value, expected_result',
+    [
+        (True, True),  # Test case for has_voted=True
+        (False, False),  # Test case for has_voted=False
+    ],
+)
+def test_has_voted(has_voted_value, expected_result):
+    voter = VoterFactory(has_voted=has_voted_value)
 
-    assert has_voted(voter.dni) is False
+    assert has_voted(voter.dni) == expected_result
 
 
 @pytest.mark.django_db
@@ -23,32 +25,20 @@ def test_has_voted_not_exits():
     assert has_voted(1234) is None
 
 
+# has_voted_percentage tests
 @pytest.mark.django_db
-def test_has_voted_percentage_when_0_voters():
-    voted_percentage = has_voted_percentage()
-
-    assert voted_percentage == pytest.approx(0, abs=0.01)
-
-
-@pytest.mark.django_db
-def test_has_voted_percentage_when_all_voters_voted():
-    VoterFactory(has_voted=True, dni=1)
-    VoterFactory(has_voted=True, dni=2)
-    VoterFactory(has_voted=True, dni=3)
-    VoterFactory(has_voted=True, dni=4)
-
-    voted_percentage = has_voted_percentage()
-
-    assert voted_percentage == pytest.approx(100, abs=0.01)
-
-
-@pytest.mark.django_db
-def test_has_voted_percentage_when_50_50():
-    VoterFactory(has_voted=True, dni=1)
-    VoterFactory(has_voted=True, dni=2)
-    VoterFactory(has_voted=False, dni=3)
-    VoterFactory(has_voted=False, dni=4)
+@pytest.mark.parametrize(
+    'voters_data, expected_percentage',
+    [
+        ([], 0),  # Test case for 0 voters
+        ([(True, 1), (True, 2), (True, 3), (True, 4)], 100),  # Test case for all voters voted
+        ([(True, 1), (True, 2), (False, 3), (False, 4)], 50),  # Test case for 50-50
+    ],
+)
+def test_has_voted_percentage(voters_data, expected_percentage):
+    for voted, dni in voters_data:
+        VoterFactory(has_voted=voted, dni=dni)
 
     voted_percentage = has_voted_percentage()
 
-    assert voted_percentage == pytest.approx(50, abs=0.01)
+    assert voted_percentage == pytest.approx(expected_percentage, abs=0.01)
